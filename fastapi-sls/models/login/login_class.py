305f80e83694d7e47bf35017c9f43bb302b8460a
luis felipe <lfdelahozfontalvo@gmail.com>
2025-09-02 15:32:15 -0500

    cambiooooooooos

--- fastapi-sls/models/login/login_class.py
+++ fastapi-sls/models/login/login_class.py
@@ -1,13 +1,10 @@
 from db import execute_query
 
 class Login:
-<<<<<<< HEAD
+
     
     def login_user(self, username: str, password: str):
-=======
 
-    def login_user(username: str, password: str):
->>>>>>> 7ecab891f2fd8ddbdc2c002bde903de2492407a0
         query = "SELECT * FROM usuarios WHERE User_name = %s AND User_password = %s"
         try:
             result = execute_query(query, (username, password), fetchone=True)
--- fastapi-sls/routers/router.py
+++ fastapi-sls/routers/router.py
@@ -2,7 +2,7 @@ from login_route import login_router
 from user_route import user_router
 from category_route import category_router
 from product_route import Product_router
-from car_route import carrito_router
+
 
 array_router = [
     login_router,
