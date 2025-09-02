cd5a6c0c78dd2c3770e1d56afcc3df2ad461982a
luis felipe <lfdelahozfontalvo@gmail.com>
2025-09-02 17:29:40 -0500

    usuario completo

--- fastapi-sls/models/user/user_class.py
+++ fastapi-sls/models/user/user_class.py
@@ -1,4 +1,5 @@
 from db import execute_query
+from fastapi.responses import JSONResponse
 
 
 class Usuario:
@@ -8,11 +9,20 @@ class Usuario:
             VALUES (%s, %s, %s, %s, %s)
         """
         try:
-            execute_query(query, (id, username, phone, email, password), commit=True)
-            return True
+                    registro=execute_query(query, (id, username, phone, email, password), commit=True)
+                    if registro:
+                        return JSONResponse(content={
+                        "success": True,
+                        "message": "Usuario registrado exitosamente"
+                })
+                    else:
+                        return JSONResponse(content={
+                        "success": False,
+                        "message": "Error al registrar usuario"
+                })
         except Exception as e:
-            print(f"Error en register_user: {e}")
-            return False
+                    print(f"Error en register_user: {e}")
+                    return False
 
 
     def update_user(self, id: int, username: str, phone: int, email: str, password: str):
@@ -22,8 +32,18 @@ class Usuario:
             WHERE User_id = %s
         """
         try:
-            execute_query(query, (username, phone, email, password, id), commit=True)
-            return True
+            usuario = execute_query(query, (username, phone, email, password, id), commit=True)
+            if usuario:
+                    return JSONResponse(content={
+                        "success": True,
+                        "message": "Usuario actualizado exitosamente"
+                         })
+            else:
+                    return JSONResponse(content={
+                            "success": False,
+                            "message": "Error al actualizar usuario"
+                        })
+           
         except Exception as e:
             print(f"Error en update_user: {e}")
             return False
@@ -32,8 +52,18 @@ class Usuario:
     def view_user(self, id: int):
         query = "SELECT * FROM usuarios WHERE User_id = %s"
         try:
-            result = execute_query(query, (id,), fetchone=True)
-            return result
+            user= execute_query(query, (id,), fetchone=True)
+            if user:
+           
+                        return JSONResponse(content={
+                            "id": user[0],
+                            "username": user[1],
+                            "phone": user[2],
+                            "email": user[3],
+                            "password": user[4]
+                        })
+            else:
+                     return JSONResponse(content={"error": "Usuario no encontrado"}, status_code=404)
         except Exception as e:
             print(f"Error en view_user: {e}")
             return None
--- fastapi-sls/routers/user_route.py
+++ fastapi-sls/routers/user_route.py
@@ -14,41 +14,14 @@ usuario = Usuario()
 
 @user_router.post("/register")
 def register(data: RegisterModel):
-    if usuario.register_user(data.id, data.username, data.phone, data.email, data.password):
-        return JSONResponse(content={
-            "success": True,
-            "message": "Usuario registrado exitosamente"
-        })
-    else:
-        return JSONResponse(content={
-            "success": False,
-            "message": "Error al registrar usuario"
-        })
+        return usuario.register_user(data.id, data.username, data.phone, data.email, data.password)
 
 
 @user_router.get("/view/{id}")
 def get_user_json(id: int):
-    user =usuario.view_user(id)
-    if not user:
-        return JSONResponse(content={"error": "Usuario no encontrado"}, status_code=404)
-    return JSONResponse(content={
-        "id": user[0],
-        "username": user[1],
-        "phone": user[2],
-        "email": user[3],
-        "password": user[4]
-    })
+    return usuario.view_user(id)
 
 
 @user_router.put("/update")
 def updateP(data: UpdateUserModel):
-    if usuario.update_user(data.id, data.username, data.phone, data.email, data.password):
-        return JSONResponse(content={
-            "success": True,
-            "message": "Usuario actualizado exitosamente"
-        })
-    else:
-        return JSONResponse(content={
-            "success": False,
-            "message": "Error al actualizar usuario"
-        })
+   return usuario.update_user(data.id, data.username, data.phone, data.email, data.password)
