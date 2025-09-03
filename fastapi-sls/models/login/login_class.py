ab3e69303ead4ee77a9f6a924c0a883559642d0f
luis felipe <lfdelahozfontalvo@gmail.com>
2025-09-03 10:54:47 -0500

    me queda un %

--- fastapi-sls/models/login/login_class.py
+++ fastapi-sls/models/login/login_class.py
@@ -7,23 +7,18 @@ class Login:
         query = "SELECT * FROM usuarios WHERE User_name = %s AND User_password = %s"
         try:
             user = execute_query(query, (username, password), fetchone=True)
-
-            if user:
-                user_id = user[0]
-                return JSONResponse(content={
+            user_id = user[0]
+            return JSONResponse(content={
                     "success": True,
                     "message": f"Bienvenid@ {username}",
                     "data": {"user_id": user_id}
                 })
-            else:
-                return JSONResponse(content={
+ 
+              
+        except Exception as e:
+            print(f"Error en login_user: {e}")
+            return JSONResponse(content={
                     "success": False,
                     "message": "Usuario o contraseña incorrectos"
                 })
 
-        except Exception as e:
-            print(f"Error en login_user: {e}")
-            return JSONResponse(content={
-                "success": False,
-                "message": "Error interno en el servidor"
-            }, status_code=500)
