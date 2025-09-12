1401c8c7e9571b659435f0f2abed7187f3836c26
luis felipe <lfdelahozfontalvo@gmail.com>
2025-09-12 09:10:51 -0500

    3

--- fastapi-sls/routers/carrito_router.py
+++ fastapi-sls/routers/carrito_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from models.carrito.carrito_entity import CarritoEntity, EliminarProducto
+from models.carrito.carrito_entity import CarritoEntity
 from models.carrito.carrito_class import CarritoClass,Updatecantidad
 from services.carrito_service import eliminar_producto, obtener_carrito_usuario
 
@@ -31,11 +31,6 @@ def eliminar(detalle_id: int, car_id: int):
 @carrito_router.put("/actualizar/{detalle_id}/{car_id}")
 def actualizar(detalle_id: int,car_id:int, up:Updatecantidad):
     return carro.actualizar_producto(detalle_id,car_id,up)
-
-@carrito_router.put("/finalizar/{car_id}")
-def finalizar(car_id: int):
-    return finalizar_compra(car_id)
-
 
 
 
