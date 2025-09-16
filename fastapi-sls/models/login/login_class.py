7dfc8391cab0c782404baf2af98b80f37640095a
luis felipe <lfdelahozfontalvo@gmail.com>
2025-09-16 16:55:30 -0500

    .

--- fastapi-sls/models/login/login_class.py
+++ fastapi-sls/models/login/login_class.py
@@ -10,7 +10,6 @@ class Login:
         try:
             confirmacion= execute_query(query, (data.user_cc, data.password), fetchone=True)
             
-
             if confirmacion:
                 resultado = val.verificar_user_type_por_password(data.password)
                 user_type = resultado["user_type"]
@@ -18,32 +17,24 @@ class Login:
                      return JSONResponse(content={
                             "success": True,
                             "message": "Bienvenido Admin",
-                    
+                            "user_type": 1
                 })
                 else:
-                    
                     query_name = "SELECT user_name FROM usuarios WHERE user_cc = %s and user_type = '2'"
                     username = execute_query(query_name,(data.user_cc,),fetchone=True)
+                    
+                    nombre_usuario = username[0] if username else "Usuario"
                    
-                    
                     return JSONResponse(content={
-                         
                         "success": True,
-                        "message": {"Bienvenid@": username},
-                        "": {"Identificacion":data.user_cc}
+                        "message": f"Bienvenid@: {nombre_usuario}",
+                        "user_type": 2,
+                        "user_cc": data.user_cc
                     })
-                    
             else:
                  return JSONResponse(content={
                                     "success": False,
                                     "message": "Usuario o contraseña incorrectos"
                                 })
-
-        except Exception as e:
-                           
+        except Exception as e:        
                 print(f"Error en login_user: {e}")
-                
-
-
- 
-     
